9b90b9ebd2e080c070837c5e83424cfddb0ba5c5
Jake Kaufman <jakeman@foursquare.com>
2013-11-19 22:17:04 +0000

    Change to use a word list
    Fix the randomize location function to not write words inside of a "border" of
    10% of the image size. This should help prevent out of bound words.

--- doger.sh
+++ doger.sh
@@ -1,14 +1,23 @@
 #!/bin/bash
 
+word_list="fluff doge haha cute deog shibe"
+
+prefix_list=( so much very such )
+
+word_list="$word_list wow wow wow"
+
 point_size=18
-font="/Library/Fonts/Comic Sans MS.ttf" 
+font="/Library/Fonts/Comic Sans MS.ttf"
 color="rgb(0,0,255)"
 input_image="odoge.jpg"
 output_image="out.jpg"
 geometry=`gm identify odoge.jpg | cut -d" " -f3 | cut -d"+" -f1`
 width=`cut -d"x" -f1 <<< $geometry`
 height=`cut -d"x" -f2 <<< $geometry`
+w_border=$((${width}/10))
+h_border=$((${height}/10))
 echo ${width}x${height}
+echo ${w_border}x${h_border}
 
 function edit_image() {
   gm convert -font "$font" -pointsize "$point_size" -fill "$color" -draw "$draw" "$input_image" "$output_image"
@@ -22,28 +31,18 @@ function randomize_color() {
 }
 
 function randomize_location() {
-  x=$((${RANDOM}%${width}))
-  y=$((${RANDOM}%${height}))
+  x=$((${RANDOM}%(${width}-2*${w_border})+${w_border}))
+  y=$((${RANDOM}%(${height}-2*${h_border})+${h_border}))
 
   location="${x},${y}"
 }
 
+for word in $word_list
+do
 randomize_location
-word="wow"
-draw="text $location $word" 
+randomize_color
+draw="text $location $word"
+echo "Adding $word at $draw"
 edit_image
-
 input_image="$output_image"
-
-randomize_location
-word="wow"
-draw="text $location $word" 
-randomize_color
-edit_image
-
-randomize_location
-word="wow"
-draw="text $location $word" 
-randomize_color
-edit_image
-
+done
